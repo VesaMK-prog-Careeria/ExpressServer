b93f944d90c8b1ac46201313f849fb0d16b943c1
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2024-01-31 17:09:57 +0200

    pin koodin kysely

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Express.js palvelin //
 const path = require('path')
 const express = require('express')
+const fs = require('fs').promises
 
 const app = express()
 
@@ -9,6 +10,19 @@ const herkut = require('./Herkut.json')
 //GET ALL etsitään kaikki herkut jsonista
 app.get('/api/herkut', (req, res) => {
     res.json(herkut)
+})
+
+// Pinkoodin lukeminen txt tiedostosta palvelimelta ja lähettäminen selaimelle
+app.get('/api/getpin', async (req, res) => {
+    try {
+        // Luetaan pin.txt tiedosto palvelimelta
+        const savedPin = await fs.readFile('pin.txt', 'utf8')
+        // Lähetetään pin selaimelle
+        res.send(savedPin)
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal server error')
+    }
 })
 
 // Tehdään polkumääritys public kansioon
